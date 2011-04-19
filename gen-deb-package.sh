bd0c84a5199cdf7fc05e2a6b3ea0d962a16c0137
Robin Gareus <robin@gareus.org>
2011-04-19 17:55:57 +0200

    updates to 1.8.0

--- gen-deb-package.sh
+++ gen-deb-package.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-VERSION=1.7.0-GA
-DEBVERSION=1.7.0
-DLURL=http://sourceforge.net/projects/airtime/files/airtime-${VERSION}.tar.gz/download
+VERSION=1.8.0
+DEBVERSION=1.8.0
+DLURL=http://sourceforge.net/projects/airtime/files/${VERSION}/airtime-${VERSION}.tar.gz/download
 MIRRORPATH=/tmp
 BUILDDEST=/tmp/airtime-${DEBVERSION}/
 DEBDIR=`pwd`/debian
@@ -13,69 +13,77 @@ if [ ! -f ${MIRRORPATH}/airtime-${VERSION}.tar.gz ]; then
 		${DLURL} 
 fi
 
+if [ ! -f ${MIRRORPATH}/airtime-${VERSION}-audiosamples.tar.gz ]; then
+	curl -L \
+		-o ${MIRRORPATH}/airtime-${VERSION}-audiosamples.tar.gz \
+		http://sourceforge.net/projects/airtime/files/${VERSION}/airtime-${VERSION}-audiosamples.tar.gz/download
+fi
+
+
 #delete prev. deb package files
+echo "cleaning up."
 rm -f ${BUILDDEST}/../airtime_${DEBVERSION}*
 rm -rf ${BUILDDEST}
 
 mkdir -p ${BUILDDEST}
 
 cd ${BUILDDEST} || exit
+echo "unzipping.."
+
 tar xzf ${MIRRORPATH}/airtime-${VERSION}.tar.gz || exit
+tar xzf ${MIRRORPATH}/airtime-${VERSION}-audiosamples.tar.gz || exit
 cp -a $DEBDIR debian || exit
 
-# FIXES fo 1.7 #############
+pwd
+
+# FIXES fo 1.8 #############
 find airtime \( \
 	   -iname "*.php" -o -iname "*.js" -o -iname "*.mp3" \
 	-o -iname "*.xsd" -o -iname "*.xsl" -o -iname "*.xml" \
 	-o -iname "*.cfg.*" -o -iname "*.txt" \
 	\) -exec chmod -x "{}" \;
 
-chmod +x airtime/install/airtime-user.php
-chmod +x airtime/library/php-amqplib/demo/amqp_airtime_consumer.php
-chmod +x airtime/library/php-amqplib/demo/amqp_consumer.php
-chmod +x airtime/library/php-amqplib/demo/amqp_publisher.php
-chmod +x airtime/library/propel/generator/pear/pear-propel-gen
 chmod +x airtime/python_apps/api_clients/api_client.py
-chmod +x airtime/python_apps/pypo/install/pypo-daemontools-liquidsoap.sh
-chmod +x airtime/python_apps/pypo/install/pypo-daemontools-logger.sh
-chmod +x airtime/python_apps/pypo/install/pypo-daemontools.sh
-chmod +x airtime/python_apps/pypo/install/pypo-install.py
-chmod +x airtime/python_apps/pypo/install/pypo-start.py
-chmod +x airtime/python_apps/pypo/install/pypo-stop.py
-chmod +x airtime/python_apps/pypo/install/pypo-uninstall.py
+chmod +x airtime/python_apps/pypo/install/pypo-liquidsoap-daemontools-logger.sh
 chmod +x airtime/python_apps/pypo/scripts/library/liquidsoap.gentoo.initd
 
-chmod +x airtime/python_apps/pypo/scripts/library/liquidsoap.initd
-chmod +x airtime/python_apps/pypo/util/status.py
-chmod +x airtime/python_apps/show-recorder/install/recorder-daemontools-logger.sh
-chmod +x airtime/python_apps/show-recorder/install/recorder-daemontools.sh
-chmod +x airtime/python_apps/show-recorder/install/recorder-install.py
-chmod +x airtime/python_apps/show-recorder/install/recorder-start.py
-chmod +x airtime/python_apps/show-recorder/install/recorder-stop.py
-chmod +x airtime/python_apps/show-recorder/install/recorder-uninstall.py
-chmod +x airtime/python_apps/show-recorder/testrecordscript.py
-chmod +x airtime/application/models/cron/croncall.php
+chmod +x airtime/airtime_mvc/application/models/cron/croncall.php
+chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_airtime_consumer.php
+chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_consumer.php
+chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_publisher.php
+chmod +x airtime/airtime/install/airtime-user.php
 
-rm airtime/public/css/colorpicker/images/Thumbs.db
+# invalid interpreter #!/usr/local/bin/python != /usr/bin/python
+echo "#!/usr/bin/python" > airtime/python_apps/show-recorder/recorder.py2
+cat airtime/python_apps/show-recorder/recorder.py >> airtime/python_apps/show-recorder/recorder.py2
+mv airtime/python_apps/show-recorder/recorder.py2 airtime/python_apps/show-recorder/recorder.py
+chmod +x airtime/python_apps/show-recorder/recorder.py
+
+
+# no hash-bang
+chmod -x airtime/python_apps/pypo/pypo-api-validator.py
+chmod -x airtime/python_apps/pypo/pypopush.py
+chmod -x airtime/python_apps/pypo/dls/__init__.py
+chmod -x airtime/python_apps/pypo/dls/__init__.py
+chmod -x airtime/python_apps/pypo/pypofetch.py
+chmod -x airtime/python_apps/pypo/pypo-cue-in-validator.py
+chmod -x airtime/python_apps/pypo/util/__init__.py
+chmod -x airtime/airtime_mvc/library/propel/generator/bin/propel-gen.bat
 
 # these are all moved to debian/copyright
 rm airtime/python_apps/pypo/LICENSE
-rm airtime/library/php-amqplib/LICENSE
-rm airtime/library/phing/LICENSE
-rm airtime/library/propel/LICENSE
+rm airtime/airtime_mvc/library/php-amqplib/LICENSE
+rm airtime/airtime_mvc/library/phing/LICENSE
+rm airtime/airtime_mvc/library/propel/LICENSE
+
 find airtime/audio_samples -iname "LICENSE.txt" -exec rm "{}" \;
 
-# unusual interpreter: #!/sbin/runscript, /usr/local/bin/python
 rm airtime/python_apps/pypo/scripts/library/liquidsoap.gentoo.initd
-rm python_apps/pypo/scripts/library/liquidsoap.gentoo.initd.in
 rm airtime/python_apps/pypo/scripts/library/liquidsoap.gentoo.initd.in
 rm airtime/python_apps/show-recorder/testrecordscript.py 
-rm airtime/library/propel/generator/bin/propel-gen.bat
+rm airtime/airtime_mvc/library/propel/generator/bin/propel-gen.bat
 
-# missing hash-bang
-chmod -x airtime/python_apps/pypo/pypo-api-validator.py
 
-echo "RewriteBase /" >> airtime/public/.htaccess
 
 #############################
 

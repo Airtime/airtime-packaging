fc5b14cf2deaf755011a05ffc586f7d8e162efc0
Daniel James <daniel@64studio.com>
2011-08-31 11:53:40 +0100

    Tidied up for internal packaging

--- gen-deb-package.sh
+++ gen-deb-package.sh
@@ -14,13 +14,6 @@ if [ ! -f ${MIRRORPATH}/airtime-${VERSION}.tar.gz ]; then
 		${DLURL}
 fi
 
-#we no longer have an airtime-audiosamples package for each release
-#if [ ! -f ${MIRRORPATH}/airtime-audiosamples-${VERSION}.tar.gz ]; then
-#	curl -L \
-#		-o ${MIRRORPATH}/airtime-audiosamples-${VERSION}.tar.gz \
-#		http://sourceforge.net/projects/airtime/files/${VERSION}${SFOCUSTOM}/airtime-audiosamples-${VERSION}${SFOCUSTOM}.tar.gz/download
-#fi
-
 #delete prev. deb package files
 echo "cleaning up."
 rm -f ${BUILDDEST}/../airtime_${DEBVERSION}*
@@ -32,7 +25,6 @@ cd ${BUILDDEST} || exit
 echo "unzipping.."
 
 tar xzf ${MIRRORPATH}/airtime-${VERSION}.tar.gz || exit
-#tar xzf ${MIRRORPATH}/airtime-audiosamples-${VERSION}.tar.gz || exit
 cp -a $DEBDIR debian || exit
 
 mv -vi airtime-${VERSION}${SFOCUSTOM} airtime
@@ -61,24 +53,6 @@ chmod +x airtime/utils/airtime-user.php
 chmod +x airtime/utils/phone_home_stat.php
 chmod +x airtime/utils/rivendell-converter.sh
 
-# we don't use env in a Debian package
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/api_clients/api_client.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/media-monitor/install/media-monitor-install.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/media-monitor/install/media-monitor-uninstall.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/pypo/install/pypo-install.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/pypo/install/pypo-uninstall.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/pypo/pypo-cli.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/pypo/pypo-notify.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/pypo/util/status.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/show-recorder/install/recorder-install.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/python_apps/show-recorder/install/recorder-uninstall.py
-#sed -i 's:#!/usr/bin/env python:#!/usr/bin/python2.6:g' airtime/utils/serbianLatinToCyrillicConverter.py
-
-# invalid interpreter #!/usr/local/bin/python != /usr/bin/python
-#sed -i 's:#!/usr/local/bin/python:#!/usr/bin/python2.6:g' airtime/python_apps/media-monitor/MediaMonitor.py
-#sed -i 's:#!/usr/local/bin/python:#!/usr/bin/python2.6:g' airtime/python_apps/show-recorder/recorder.py
-#sed -i 's:#!/usr/local/bin/python:#!/usr/bin/python2.6:g' airtime/utils/airtime-import/airtime-import.py
-
 # force scripts to use the virtualenv in bundled install
 sed -i 's:#!/usr/local/bin/python:#!/usr/bin/env python:g' airtime/python_apps/media-monitor/MediaMonitor.py
 sed -i 's:#!/usr/local/bin/python:#!/usr/bin/env python:g' airtime/python_apps/show-recorder/recorder.py
@@ -97,9 +71,6 @@ rm airtime/python_apps/pypo/LICENSE
 rm airtime/airtime_mvc/library/php-amqplib/LICENSE
 rm airtime/airtime_mvc/library/phing/LICENSE
 rm airtime/airtime_mvc/library/propel/LICENSE
-
-# we no longer have an airtime-audiosamples package for each release
-#find airtime/audio_samples -iname "LICENSE.txt" -exec rm "{}" \;
 
 # we don't need a Gentoo init script in a Debian package
 rm airtime/python_apps/pypo/liquidsoap_scripts/library/liquidsoap.gentoo.initd.in

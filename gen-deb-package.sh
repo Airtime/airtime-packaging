4b8c8555e2abab30451f943b4ccf4aca844fd961
Daniel James <daniel@64studio.com>
2011-09-15 10:30:06 +0100

    Updates for 1.9.4-RC3 release

--- gen-deb-package.sh
+++ gen-deb-package.sh
@@ -1,14 +1,14 @@
 #/bin/sh
 
-VERSION=1.9.3
-SFOCUSTOM=""
-DEBVERSION=1.9.3
+VERSION=1.9.4
+SFOCUSTOM="-RC3"
+DEBVERSION=1.9.4
 DLURL=http://sourceforge.net/projects/airtime/files/${DEBVERSION}/airtime-${VERSION}${SFOCUSTOM}.tar.gz/download
 MIRRORPATH=/tmp
 BUILDDEST=/tmp/airtime-${DEBVERSION}/
 DEBDIR=`pwd`/debian
 
-if [ ! -f ${MIRRORPATH}/airtime-${VERSION}.tar.gz ]; then
+if [ ! -f ${MIRRORPATH}/airtime-${VERSION}${SFOCUSTOM}.tar.gz ]; then
 	curl -L \
 		-o ${MIRRORPATH}/airtime-${VERSION}.tar.gz \
 		${DLURL}
@@ -24,48 +24,33 @@ mkdir -p ${BUILDDEST}
 cd ${BUILDDEST} || exit
 echo "unzipping.."
 
-tar xzf ${MIRRORPATH}/airtime-${VERSION}.tar.gz || exit
+tar xzf ${MIRRORPATH}/airtime-${VERSION}${SFOCUSTOM}.tar.gz || exit
 cp -a $DEBDIR debian || exit
 
-mv -vi airtime-${VERSION}${SFOCUSTOM} airtime
+mv -vi airtime-${VERSION} airtime
 pwd
 
-# FIXES for 1.9 #############
-find airtime \( \
-	   -iname "*.php" -o -iname "*.js" -o -iname "*.mp3" \
-	-o -iname "*.xsd" -o -iname "*.xsl" -o -iname "*.xml" \
-	-o -iname "*.cfg.*" -o -iname "*.txt" \
+# FIXES for 1.9.4 #############
+find airtime/airtime_mvc/public/js/datatables/unit_testing/tests_onhold \( \
+	   -iname "*.js" \
 	\) -exec chmod -x "{}" \;
 
-chmod +x airtime/airtime_mvc/application/models/cron/croncall.php
-chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_airtime_consumer.php
-chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_consumer.php
-chmod +x airtime/airtime_mvc/library/php-amqplib/demo/amqp_publisher.php
+# script-not-executable
+chmod +x airtime/python_apps/media-monitor/install/media-monitor-install.py
+chmod +x airtime/python_apps/media-monitor/install/media-monitor-uninstall.py
+chmod +x airtime/python_apps/pypo/install/pypo-install.py
+chmod +x airtime/python_apps/pypo/install/pypo-uninstall.py
+chmod +x airtime/python_apps/pypo/pypo-cli.py
+chmod +x airtime/python_apps/pypo/pypo-notify.py
+chmod +x airtime/python_apps/pypo/util/status.py
+chmod +x airtime/python_apps/show-recorder/install/recorder-install.py
+chmod +x airtime/python_apps/show-recorder/install/recorder-uninstall.py
+chmod +x airtime/utils/serbianLatinToCyrillicConverter.py
 
-chmod +x airtime/install_minimal/3rd_party/setuptools-0.6c11-py2.6.egg
-
-chmod +x airtime/python_apps/api_clients/api_client.py
-chmod +x airtime/python_apps/media-monitor/MediaMonitor.py
-chmod +x airtime/python_apps/show-recorder/recorder.py
-
-chmod +x airtime/utils/airtime-import/airtime-import.py
-chmod +x airtime/utils/airtime-user.php
-chmod +x airtime/utils/phone_home_stat.php
-chmod +x airtime/utils/rivendell-converter.sh
-
-# force scripts to use the virtualenv in bundled install
-sed -i 's:#!/usr/local/bin/python:#!/usr/bin/env python:g' airtime/python_apps/media-monitor/MediaMonitor.py
-sed -i 's:#!/usr/local/bin/python:#!/usr/bin/env python:g' airtime/python_apps/show-recorder/recorder.py
-sed -i 's:#!/usr/local/bin/python:#!/usr/bin/env python:g' airtime/utils/airtime-import/airtime-import.py
-
-# no hash-bang
-chmod -x airtime/python_apps/pypo/pypofetch.py
-chmod -x airtime/python_apps/pypo/pypopush.py
-chmod -x airtime/python_apps/pypo/liquidsoap_scripts/library/tests/LS354-1.liq
-chmod -x airtime/python_apps/pypo/liquidsoap_scripts/library/tests/LS354-2.liq
-chmod -x airtime/python_apps/pypo/test/pypo-api-validator.py
-chmod -x airtime/python_apps/pypo/util/__init__.py
-chmod -x airtime/utils/airtime-user.php
+# executable-not-elf-or-script
+chmod -x airtime/airtime_mvc/application/models/cron/CronJob.php
+chmod -x airtime/airtime_mvc/application/models/cron/Crontab.php
+chmod -x airtime/airtime_mvc/application/models/cron/Cron.php
 
 # these are all moved to debian/copyright
 rm airtime/python_apps/pypo/LICENSE
@@ -83,10 +68,10 @@ rm airtime/airtime_mvc/library/propel/generator/bin/propel-gen.bat
 mv airtime/Changelog airtime/changelog
 
 # Disable install script check for Debian package, we don't need it
-sed -i 's:DEB=$(dpkg:# DEB=$(dpkg:g' airtime/install_minimal/airtime-install
-sed -i 's\"$DEB" = "Status: install ok installed"\-f /var/lib/dpkg/info/airtime.config\g' airtime/install_minimal/airtime-install
-sed -i 's: Please use the debian package to upgrade.:..:g' airtime/install_minimal/airtime-install
-sed -i 's:exit 1:# We do not exit here:g' airtime/install_minimal/airtime-install
+sed -i '6s:DEB=$(dpkg:# DEB=$(dpkg:g' airtime/install_minimal/airtime-install
+sed -i '8s\"$DEB" = "Status: install ok installed"\-f /var/lib/dpkg/info/airtime.config\g' airtime/install_minimal/airtime-install
+sed -i '9s: Please use the debian package to upgrade.:..:g' airtime/install_minimal/airtime-install
+sed -i '10s:exit 1:# We do not exit here:g' airtime/install_minimal/airtime-install
 
 #############################
 

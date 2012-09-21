de6324f3b9b2a88f02892e9910e379b896298f77
Daniel James <daniel@64studio.com>
2012-09-21 16:43:45 +0100

    Changes for upstream 2.2.0-rc1 release

--- gen-deb-package.sh
+++ gen-deb-package.sh
@@ -1,8 +1,8 @@
 #/bin/sh
+# Script for generating official Airtime packages
 
-VERSION=2.1.3
-SFOCUSTOM="-ga"
-#DEBVERSION=2.1.3
+VERSION=2.2.0
+SFOCUSTOM="-rc1"
 DLURL=http://sourceforge.net/projects/airtime/files/${VERSION}${SFOCUSTOM}/airtime-${VERSION}${SFOCUSTOM}.tar.gz/download
 MIRRORPATH=/tmp
 BUILDDEST=/tmp/airtime-${VERSION}/
@@ -30,7 +30,7 @@ cp -a $DEBDIR debian || exit
 mv -vi airtime-${VERSION} airtime
 pwd
 
-# FIXES for 2.1.3 #############
+# FIXES for 2.2.0 #############
 
 # these are all moved to debian/copyright
 rm airtime/python_apps/pypo/LICENSE
@@ -39,31 +39,8 @@ rm airtime/airtime_mvc/library/phing/LICENSE
 rm airtime/airtime_mvc/library/propel/LICENSE
 rm airtime/airtime_mvc/library/soundcloud-api/README.md
 
-# Disable install script check for Debian package, we don't need it
-sed -i '11s:DEB=$(dpkg:# DEB=$(dpkg:g' airtime/install_minimal/airtime-install
-sed -i '13s\"$DEB" = "Status: install ok installed"\-f /var/lib/dpkg/info/airtime.config\g' airtime/install_minimal/airtime-install
-sed -i '14s: Please use the debian package to upgrade.:..:g' airtime/install_minimal/airtime-install
-sed -i '15s:exit 1:# We do not exit here:g' airtime/install_minimal/airtime-install
-
 # Remove Liquidsoap binaries
 rm -r airtime/python_apps/pypo/liquidsoap_bin/
-
-# Disable installation of Liquidsoap binaries
-sed -i '84s:print:#print:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '86s:binary_path:#binary_path:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '88s:try:#try:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '89s:open:#open:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '91s:try:#try:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '92s:os.remove:#os.remove:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '93s:except:#except:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '95s:pass:#pass:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '97s:os.symlink:#os.symlink:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '98s:except:#except:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '99s:    """:""":g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '107s:    """:""":g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '108s:print:#print:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '109s:print:#print:g' airtime/python_apps/pypo/install/pypo-initialize.py
-sed -i '110s:sys.exit(1):#sys.exit(1):g' airtime/python_apps/pypo/install/pypo-initialize.py
 
 #Remove phing library
 rm -r airtime/airtime_mvc/library/phing/

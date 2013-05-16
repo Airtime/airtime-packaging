4762784e0ae469cdde9e37c53a82c75f6d7795f4
Daniel James <daniel@64studio.com>
2013-05-16 12:12:47 +0100

    Updates for Liquidsoap 1.1.1 packaging

--- gen-liquidsoaps.sh
+++ gen-liquidsoaps.sh
@@ -3,7 +3,7 @@
 # Packaging script for Liquidsoap binaries. See https://wiki.sourcefabric.org/display/CC/Packaging+Liquidsoap
 # Please download the tarball of Liquidsoap binaries to $MIRRORPATH before running this script
 
-LIQUIDSOAP_VERSION=1.1.0
+LIQUIDSOAP_VERSION=1.1.1
 LIQUIDSOAP_CUSTOM="sfo-1"
 MIRRORPATH=/tmp
 
@@ -21,18 +21,18 @@ cd ${MIRRORPATH}/
 # Test that the Liquidsoap binaries are all present
 
 if test ! \( -d liquidsoap/bin \
-	-a -f liquidsoap/bin/liquidsoap_squeeze_amd64 \
-	-a -f liquidsoap/bin/liquidsoap_squeeze_i386  \
-        -a -f liquidsoap/bin/liquidsoap_wheezy_amd64 \
-        -a -f liquidsoap/bin/liquidsoap_wheezy_i386  \
 	-a -f liquidsoap/bin/liquidsoap_lucid_amd64 \
-	-a -f liquidsoap/bin/liquidsoap_lucid_i386  \
-	-a -f liquidsoap/bin/liquidsoap_oneiric_amd64 \
-	-a -f liquidsoap/bin/liquidsoap_oneiric_i386 \
+	-a -f liquidsoap/bin/liquidsoap_lucid_i386 \
 	-a -f liquidsoap/bin/liquidsoap_precise_amd64 \
 	-a -f liquidsoap/bin/liquidsoap_precise_i386 \
         -a -f liquidsoap/bin/liquidsoap_quantal_amd64 \
-        -a -f liquidsoap/bin/liquidsoap_quantal_i386 \) \
+        -a -f liquidsoap/bin/liquidsoap_quantal_i386 \
+        -a -f liquidsoap/bin/liquidsoap_raring_amd64 \
+        -a -f liquidsoap/bin/liquidsoap_raring_i386 \
+        -a -f liquidsoap/bin/liquidsoap_squeeze_amd64 \
+        -a -f liquidsoap/bin/liquidsoap_squeeze_i386 \
+        -a -f liquidsoap/bin/liquidsoap_wheezy_amd64 \
+        -a -f liquidsoap/bin/liquidsoap_wheezy_i386 \) \
 	; then
 echo "ERROR: liquidsoap binaries not present in ${MIRRORPATH}/liquidsoap/bin/"
 exit 1
@@ -41,8 +41,8 @@ fi
 # Use these lines to build for Debian squeeze
 
         dpkg-source -b liquidsoap
+        pbuilder-dist squeeze build liquidsoap_${LIQUIDSOAP_VERSION}~squeeze~${LIQUIDSOAP_CUSTOM}.dsc
         pbuilder-dist squeeze i386 build liquidsoap_${LIQUIDSOAP_VERSION}~squeeze~${LIQUIDSOAP_CUSTOM}.dsc
-        pbuilder-dist squeeze amd64 build liquidsoap_${LIQUIDSOAP_VERSION}~squeeze~${LIQUIDSOAP_CUSTOM}.dsc
 
 # Set the correct distro name in the package changelog
 
@@ -58,11 +58,11 @@ head -n1 liquidsoap/debian/changelog
 
 # Use these lines to build for other distros
 
-#for dist in lucid oneiric precise quantal wheezy; do
+#for dist in lucid precise quantal raring wheezy; do
 #	set_dist $dist
 #	dpkg-source -b liquidsoap
+#	pbuilder-dist $dist build liquidsoap_${LIQUIDSOAP_VERSION}~${dist}~${LIQUIDSOAP_CUSTOM}.dsc
 #	pbuilder-dist $dist i386 build liquidsoap_${LIQUIDSOAP_VERSION}~${dist}~${LIQUIDSOAP_CUSTOM}.dsc
-#	pbuilder-dist $dist amd64 build liquidsoap_${LIQUIDSOAP_VERSION}~${dist}~${LIQUIDSOAP_CUSTOM}.dsc
 #done
 
 CHANGES=`ls -t ~/pbuilder/*_result/liquidsoap_*.changes | head -n 12`

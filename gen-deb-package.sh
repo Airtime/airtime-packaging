3c1f1af2107238c4189b98c446dd3af6648e7fb3
Daniel James <daniel@64studio.com>
2015-01-22 13:00:33 +0000

    Initial packaging for Airtime 2.5.2

--- gen-deb-package.sh
+++ gen-deb-package.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 # Script for generating official Airtime packages
 
-GITTAG=airtime-2.5.1a
-VERSION=2.5.1
+GITTAG=airtime-2.5.2-rc1
+VERSION=2.5.2
 SFOCUSTOM=""
 DLURL=https://github.com/sourcefabric/Airtime/archive/${GITTAG}${SFOCUSTOM}.tar.gz
 MIRRORPATH=/tmp
@@ -34,29 +34,22 @@ cp -a $DEBDIR debian || exit
 mv -vi Airtime-${GITTAG}* airtime
 pwd
 
-# FIXES for 2.5.1a #############
+# FIXES for 2.5.2 #############
 
 # these are all moved to debian/copyright
-rm airtime/python_apps/pypo/LICENSE
 rm airtime/airtime_mvc/library/php-amqplib/CREDITS
 rm airtime/airtime_mvc/library/php-amqplib/LICENSE
 rm airtime/airtime_mvc/library/php-amqplib/README.md
 rm airtime/airtime_mvc/library/phing/LICENSE
 rm airtime/airtime_mvc/library/propel/LICENSE
 rm airtime/airtime_mvc/library/soundcloud-api/README.md
+rm airtime/python_apps/pypo/pypo/LICENSE
 
 #Remove phing library
 rm -r airtime/airtime_mvc/library/phing/
 
 #Remove ZFDebug
 rm -r airtime/airtime_mvc/library/ZFDebug/
-
-#Strip un-needed install scripts
-rm -r airtime/install_full/
-
-#Strip snapshot generation files
-rm airtime/gen-snapshot.sh
-rm -r airtime/debian/
 
 #Remove dev tools and files
 rm -r airtime/dev_tools/
@@ -71,16 +64,21 @@ rm -r airtime/airtime_mvc/library/propel/test
 rm airtime/airtime_mvc/library/propel/INSTALL
 rm airtime/airtime_mvc/library/propel/WHATS_NEW
 
+#Fix permissions
+chmod -x airtime/installer/lib/requirements-ubuntu-trusty.apt
+chmod -x airtime/installer/lib/requirements-ubuntu-precise.apt
+
 #Remove Mutagen patches
-rm -r airtime/python_apps/python-virtualenv/patches/mutagen
+#rm -r airtime/python_apps/python-virtualenv/patches/mutagen
 
 #############################
 
 #Uncomment to build an orig tarball for a new upstream release
-#cd ../
-#tar czf airtime_${VERSION}.orig.tar.gz  airtime-${VERSION}/airtime/
-#cd ${BUILDDEST} || exit
+cd ../
+tar czf airtime_${VERSION}.orig.tar.gz  airtime-${VERSION}/airtime/
+cd ${BUILDDEST} || exit
 
+#Build a signed package with the Sourcefabric key
 debuild -k174C1854 $@ || exit
 
 ls -l ${MIRRORPATH}/airtime*deb
